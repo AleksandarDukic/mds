27fc49e7234b917952459e8dff43ce3887b19ed2
AleksandarDukic <aleksandar.z.dukic@gmail.com>
2025-03-22 02:49:29 +0100

    Added Kanban Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { TodoPageComponent } from "./pages/todo-page/todo-page.component";
+import { KanbanPageComponent } from "./pages/kanban-page/kanban-page.component";
 
 
 const routes: Routes = [
   { path: "", component: TodoPageComponent },
   { path: "todos", component: TodoPageComponent },
+  { path: "kanban", component: KanbanPageComponent },
   { path: "**", redirectTo: "" },
 ];
 
---
+++ src/app/pages/kanban-page/kanban-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-kanban-page",
+  template: `
+    <div class="container">
+      <div class="card">
+        kanban
+      </div>
+
+    </div>
+  `,
+  styles: [],
+})
+export class KanbanPageComponent {}
